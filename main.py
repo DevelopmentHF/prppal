df3be9c149c518e4fc8052d521659dae363e999e
Henry Fielding <hfielding@student.unimelb.edu.au>
2025-03-21 22:12:55 +1100

    Add timeout for user feedback

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from prppal.data_extraction import extract
 from prppal.analysis import analyse
 from rich.text import Text
 from prppal.console import console
+import time
 
 def main():
     
     console.log(Text("Started prppal...\n", style="green"))
-
+    time.sleep(0.25)
     parser = argparse.ArgumentParser(description="Add file names to perform PRP analysis on.")
     parser.add_argument("--patrols", type=str, help=".csv file containing patrolling data")
     parser.add_argument("--awards", type=str, help=".csv file containing qualification and award data")
--- prppal/data_extraction.py
+++ prppal/data_extraction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from rich.text import Text
 from prppal.console import console
+import time
 
 
 def extract(filepath_args):
@@ -10,12 +11,14 @@ def extract(filepath_args):
     message.append(" & ", style="yellow")
     message.append(filepath_args.awards + "\n", style="bold green")
     console.log(message)
+    time.sleep(0.25)
 
     # load the CSV files
     patrol_df = pd.read_csv(filepath_args.patrols, header=1) # first row is garbage
     awards_df = pd.read_csv(filepath_args.awards)
 
     console.log("Files successfully loaded\n", style="green")
+    time.sleep(0.25)
 
     # merge on Member ID, the same in both csvs
     merged_df = pd.merge(patrol_df, awards_df, on="Member ID", how="inner")
